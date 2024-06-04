f2973be3299644835395415691d647b90eecad6a
ADAYLSON ARCANJO EVARISTO <adaylson.aevaristo@senacsp.edu.br>
2024-06-04 10:25:37 -0300

    primeiro exercicio

---
+++ exer1/validacao.cs
@@ -0,0 +1,11 @@
+using System;
+
+class validacao 
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bem Vindo a Função do impar ou par");
+        Console.WriteLine("Digite um numero");
+        Console.Read
+    }
+}
